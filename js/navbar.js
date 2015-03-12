6dbc21b90898de04b09f87f1544db51b9f6de432
Fabrício Godoy <skarllot@gmail.com>
2015-03-12 00:07:54 -0300

    Fixed mobile behaviour

--- js/navbar.js
+++ js/navbar.js
@@ -5,6 +5,11 @@ app.controller('navbarController', [ '$scope', '$location', '$http', function($s
         return path === $location.path();
     };
     $scope.navCollapsed = true;
+    $scope.toggleNav = function() {
+        if (window.innerWidth < 768) {
+            $scope.navCollapsed = !$scope.navCollapsed;
+        }
+    };
     
     $http.get('data/download.json')
     .success(function(result) {
