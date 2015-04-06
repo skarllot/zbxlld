f16591ab7f02d4c28496638d8dac7fdc61a54568
Fabrício Godoy <skarllot@gmail.com>
2015-04-06 15:51:14 -0300

    Fix documentation/history flicker on loading

--- js/doc/history.js
+++ js/doc/history.js
@@ -2,10 +2,12 @@ app.registerCtrl('docHistoryController', [ '$rootScope', '$scope', '$http',
                                           function($rootScope, $scope, $http) {
 
     $rootScope.title += '/History';
+    $scope.loaded = false;
 
     $http.get('data/history.json')
     .success(function(result) {
         $scope.history = result;
+        $scope.loaded = true;
     }).error(function(data) {
         console.log(data);
     });
