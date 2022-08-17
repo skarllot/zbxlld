3c0321fcf17a7da9497ccee6c8e18dfbbe80dd1d
Fabricio Godoy <skarllot@gmail.com>
2022-08-17 18:42:55 -0300

    Define configuration for publishing

--- zbxlld-win/Supplement/PerfMon/LogicalDisk.cs
+++ zbxlld-win/Supplement/PerfMon/LogicalDisk.cs
@@ -1,24 +1,3 @@
-//
-//  LogicalDisk.cs
-//
-//  Author:
-//       Fabricio Godoy <skarllot@gmail.com>
-//
-//  Copyright (c) 2013 Fabricio Godoy
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -32,12 +11,14 @@ namespace zbxlld.Windows.Supplement.PerfMon
 		private const string CounterFreeMb = "410";
 		private const long MbMult = 1048576;
 
+		private readonly Localization _localization;
+
 		// Relates Performance Monitor instance name to volume GUID.
-		private readonly Dictionary<Guid, string> _perfMonGuid;
+		private Dictionary<Guid, string>? _perfMonGuid;
 
 		public LogicalDisk(Localization localization)
 		{
-			_perfMonGuid = CreateVolumeDictionary(localization);
+			_localization = localization;
 		}
 
 		// Try to discover GUID from buggy Performance Monitor instance names.
@@ -99,6 +80,7 @@ namespace zbxlld.Windows.Supplement.PerfMon
 
 		public string? GetInstanceName(Guid guid)
 		{
+			_perfMonGuid ??= CreateVolumeDictionary(_localization);
 			_perfMonGuid.TryGetValue(guid, out string? ret);
 			return ret;
 		}
